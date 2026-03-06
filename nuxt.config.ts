11e1717a01d556c4bf317e4d99787b110dff9e9e
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2026-03-06 22:24:02 +0100

    GA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,24 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-VGN46K0G19',
+          async: true,
+        },
+        {
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VGN46K0G19');
+          `,
+        },
+      ],
+    },
+  },
   modules: ['@nuxt/content', '@nuxt/image'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
