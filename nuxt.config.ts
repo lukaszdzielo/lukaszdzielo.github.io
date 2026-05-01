f879a47e4a24ba342744934b85ab2d5c844841b4
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2026-05-01 10:14:20 +0200

    favicon and meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Lukasz Dzieło | Front-End Developer',
+      title: 'Łukasz Dzieło | Front-End Developer',
       link: [
         { rel: 'icon', type: 'image/png', href: '/favicon/favicon-96x96.png', sizes: '96x96' },
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon/favicon.svg' },
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
       meta: [
         { name: 'apple-mobile-web-app-title', content: 'lukasz.dev' },
         { name: 'description', content: 'Front-End Developer focused on Vue, responsive UI, reusable components, and maintainable interfaces across commercial and personal projects.' },
-        { name: 'author', content: 'Lukasz Dzieło' },
+        { name: 'author', content: 'Łukasz Dzieło' },
         { name: 'robots', content: 'index, follow' }
       ],
       script: [
